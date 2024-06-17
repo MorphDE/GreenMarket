895aff57918140393c50ac05c6d544cb3099b69d
Nostea <kiwiuchi@googlemail.com>
2024-06-17 21:37:49 +0200

    added Context

--- frontend/src/Components/Loadingscreen/Loadingscreen.jsx
+++ frontend/src/Components/Loadingscreen/Loadingscreen.jsx
@@ -1,27 +1,27 @@
-import { LoadingContext } from "../../Context/LoadingContext";
+import { LoadingContext } from "../../Context/Contexts";
 import "./Loadingscreen.css";
 import { useContext, useEffect } from "react";
-import LoadingImage from "../../../public/grocery.png"
+import LoadingImage from "../../../public/grocery.png";
 
 const Loadingscreen = () => {
   const { setLoading } = useContext(LoadingContext);
 
-    useEffect(() => {
-      setTimeout(() => {
-        setLoading(true);
-      }, 3000);
-    }, []);
-  
-    return (
-      <section className="loading-bg">
-        <img src={LoadingImage} alt="Loadingscreen Image" />
-        <h1 className="loading-title">GreenMarket</h1>
-        <p className="loading-slogan">EASY VEGGIE SHOPPING</p>
-        <div className="loading-container">
-            <div className="loader"></div>
-        </div>
-      </section>
-    );
-  };
- 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 3000);
+  }, []);
+
+  return (
+    <section className="loading-bg">
+      <img src={LoadingImage} alt="Loadingscreen Image" />
+      <h1 className="loading-title">GreenMarket</h1>
+      <p className="loading-slogan">EASY VEGGIE SHOPPING</p>
+      <div className="loading-container">
+        <div className="loader"></div>
+      </div>
+    </section>
+  );
+};
+
 export default Loadingscreen;
---
+++ frontend/src/Context/Contexts.jsx
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+export const LoadingContext = createContext();
+
+export const RefreshContext = createContext();
+export const UserContext = createContext();
+export const TokenContext = createContext();
