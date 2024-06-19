eeeb2a015b199b69afb204d82953754590590d0f
Nostea <kiwiuchi@googlemail.com>
2024-06-19 20:05:14 +0200

    cart Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import AppRoutes from "./AppRoutes";
-import { LoadingContext, ProductContext, RefreshContext, UserContext, TokenContext, FilterContext } from "./Context/Contexts";
+import { LoadingContext, ProductContext, RefreshContext, UserContext, TokenContext, CartContext, FilterContext } from "./Context/Contexts";
 import Loadingscreen from "./Components/Loadingscreen/Loadingscreen";
 import "@fontsource/poppins";
 
@@ -11,21 +11,24 @@ function App() {
   const [token, setToken] = useState();
   const [refreshToken, setRefreshToken] = useState();
   const [products, setProducts] = useState();
+  const [cart, setCart] = useState();
   const [filters, setFilters] = useState();
 
   return (
     <>
-      <FilterContext.Provider value={{ filters, setFilters }}>
-        <ProductContext.Provider value={{ products, setProducts }}>
-          <UserContext.Provider value={{ user, setUser }}>
-            <TokenContext.Provider value={{ token, setToken }}>
-              <RefreshContext.Provider value={{ refreshToken, setRefreshToken }}>
-                <LoadingContext.Provider value={{ loading, setLoading }}>{loading ? <AppRoutes /> : <Loadingscreen />}</LoadingContext.Provider>
-              </RefreshContext.Provider>
-            </TokenContext.Provider>
-          </UserContext.Provider>
-        </ProductContext.Provider>
-      </FilterContext.Provider>
+      <CartContext.Provider value={{ cart, setCart }}>
+        <FilterContext.Provider value={{ filters, setFilters }}>
+          <ProductContext.Provider value={{ products, setProducts }}>
+            <UserContext.Provider value={{ user, setUser }}>
+              <TokenContext.Provider value={{ token, setToken }}>
+                <RefreshContext.Provider value={{ refreshToken, setRefreshToken }}>
+                  <LoadingContext.Provider value={{ loading, setLoading }}>{loading ? <AppRoutes /> : <Loadingscreen />}</LoadingContext.Provider>
+                </RefreshContext.Provider>
+              </TokenContext.Provider>
+            </UserContext.Provider>
+          </ProductContext.Provider>
+        </FilterContext.Provider>
+      </CartContext.Provider>
     </>
   );
 }
