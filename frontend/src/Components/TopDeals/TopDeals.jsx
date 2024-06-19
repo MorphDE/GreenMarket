45f9dbf34e333452d7ac96abf1ee1180fa3d5aa8
EvenSalomon1 <even.salo@web.de>
2024-06-19 09:49:16 +0200

    topdeals fetch beginning

--- frontend/src/Components/TopDeals/TopDeals.jsx
+++ frontend/src/Components/TopDeals/TopDeals.jsx
@@ -2,20 +2,39 @@ import ProductCard from "../ProductCard/ProductCard";
 import "./TopDeals.css";
 
 const TopDeals = () => {
-    return (
-        <section className="topdeals-container">
-        <h1>Today's Deals</h1>
-        <div className="product-cards">
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
+  const { products, setProducts } = useContext(ProductContext);
 
+  useEffect(() => {
+    fetch(`${backendUrl}/api/v1/products/getAllProducts`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <section className="topdeals-container">
+      <h1>All Products</h1>
+      {products.map((item, index) => (
+        <div key={index} className="product-cards">
+          <ProductCard
+            image={item.image}
+            name={item.name}
+            price={item.price}
+            rating={item.rating}
+          />
+          {/* <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard /> */}
         </div>
-        </section>
-    );
-}
- 
+      ))}
+    </section>
+  );
+};
+
 export default TopDeals;
