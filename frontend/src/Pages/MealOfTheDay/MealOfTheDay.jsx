0c50190ff955312a5ab7ad5d083d198d8210ede2
AdrSpeer <adn.speer@gmail.com>
2024-06-24 10:33:52 +0200

    added Meal

--- frontend/src/Pages/MealOfTheDay/MealOfTheDay.jsx
+++ frontend/src/Pages/MealOfTheDay/MealOfTheDay.jsx
@@ -4,7 +4,7 @@ import { backendUrl } from "../../api/api";
 import { useAuth } from "../../Context/AuthProvider";
 import { useNavigate } from "react-router-dom";
 import "./MealOfTheDay.css";
-import Footer from './../../Components/Footer/Footer';
+import Footer from "./../../Components/Footer/Footer";
 import GoBack from "../../Components/GoBack/GoBack";
 
 const MealOfTheDay = () => {
@@ -50,97 +50,109 @@ const MealOfTheDay = () => {
 
   return (
     <>
-    <GoBack title={"Meal of the Day"}/>
-    <section className="mealoftheday">
-      <div className="recipe">
-        <h2>Veggie Casserole</h2>
-        <div className="ingredient-list">
-          <h3>Ingredients:</h3>
-          <div>
-            {ingredients.map((ingredient, index) => (
-              <p className="ingredient" key={index}>
-                {calculateIngredientQuantity(ingredient.quantity)}{" "}
-                {typeof ingredient.quantity === "number" ? "g" : ""}{" "}
-                {ingredient.name}
-              </p>
-            ))}
+      <GoBack title={"Meal of the Day"} />
+      <section className="mealoftheday">
+        <div className="recipe">
+          <h2>Veggie Casserole</h2>
+          <div className="ingredient-list">
+            <h3>Ingredients:</h3>
+            <div>
+              {ingredients.map((ingredient, index) => (
+                <p className="ingredient" key={index}>
+                  {calculateIngredientQuantity(ingredient.quantity)}{" "}
+                  {["Onion, chopped", "Garlic cloves, chopped"].includes(
+                    ingredient.name
+                  )
+                    ? ""
+                    : "g"}{" "}
+                  {ingredient.name}
+                </p>
+              ))}
+            </div>
+          </div>
+
+          <div className="instruction-steps">
+            <h3>Instructions:</h3>
+            <ol>
+              <li className="step">
+                <p className="meal-title">Prepare vegetables:</p> Thoroughly
+                wash the vegetables and cut them into bite-sized pieces,
+                depending on the type.
+              </li>
+              <li className="step">
+                <p className="meal-title">Preheat oven:</p> Preheat the oven to
+                200°C (392°F) conventional oven setting.
+              </li>
+              <li className="step">
+                <p className="meal-title">Sauté:</p> Heat olive oil in a pan and
+                sauté the chopped onion and garlic until translucent.
+              </li>
+              <li className="step">
+                <p className="meal-title">Add vegetables:</p> Add the prepared
+                vegetables and sauté for about 5-7 minutes, stirring
+                occasionally, until lightly browned. Season with salt and
+                pepper.
+              </li>
+              <li className="step">
+                <p className="meal-title">Layer:</p> Grease a large casserole
+                dish. Place a layer of potato slices on the bottom of the dish.
+                Top with a layer of the sautéed vegetables. Repeat this process
+                alternately until all ingredients are used.
+              </li>
+              <li className="step">
+                <p className="meal-title">Add cream:</p> Pour the cream or milk
+                over the vegetable casserole.
+              </li>
+              <li className="step">
+                <p className="meal-title">Sprinkle cheese:</p> Evenly sprinkle
+                the grated cheese over the vegetable casserole.
+              </li>
+              <li className="step">
+                <p className="meal-title">Bake:</p> Place the casserole dish in
+                the preheated oven and bake the vegetable casserole for about
+                30-35 minutes, until the potatoes are tender and the cheese is
+                golden brown and crispy.
+              </li>
+              <li className="step">
+                <p className="meal-title">Serve:</p> Remove the vegetable
+                casserole from the oven and let it rest for about 5 minutes
+                before serving. Garnish with fresh herbs as desired and serve
+                hot.
+              </li>
+            </ol>
+          </div>
+
+          <p className="note">
+            This veggie casserole is versatile, healthy, and can be prepared in
+            advance. You can vary the types of vegetables according to season
+            and personal preferences. Enjoy your meal!
+          </p>
+
+          <div className="person-selector">
+            <button
+              className="button-minus"
+              onClick={() =>
+                setPersons((prevPersons) => Math.max(prevPersons - 1, 1))
+              }
+            >
+              -
+            </button>
+            <span className="persons">{persons}</span>
+            <button
+              className="button-plus"
+              onClick={() => setPersons((prevPersons) => prevPersons + 1)}
+            >
+              +
+            </button>
+          </div>
+          <div className="putcart-btn">
+            <button className="put-cart" onClick={addToCart}>
+              Add to Cart
+            </button>
           </div>
         </div>
-
-        <div className="instruction-steps">
-          <h3>Instructions:</h3>
-          <ol>
-            <li className="step">
-              <p className="meal-title">Prepare vegetables:</p> Thoroughly wash the
-              vegetables and cut them into bite-sized pieces, depending on the
-              type.
-            </li>
-            <li className="step">
-              <p className="meal-title">Preheat oven:</p> Preheat the oven to 200°C (392°F)
-              conventional oven setting.
-            </li>
-            <li className="step">
-              <p className="meal-title">Sauté:</p> Heat olive oil in a pan and sauté the
-              chopped onion and garlic until translucent.
-            </li>
-            <li className="step">
-              <p className="meal-title">Add vegetables:</p> Add the prepared vegetables and
-              sauté for about 5-7 minutes, stirring occasionally, until lightly
-              browned. Season with salt and pepper.
-            </li>
-            <li className="step">
-              <p className="meal-title">Layer:</p> Grease a large casserole dish. Place a
-              layer of potato slices on the bottom of the dish. Top with a layer
-              of the sautéed vegetables. Repeat this process alternately until
-              all ingredients are used.
-            </li>
-            <li className="step">
-              <p className="meal-title">Add cream:</p> Pour the cream or milk over the
-              vegetable casserole.
-            </li>
-            <li className="step">
-              <p className="meal-title">Sprinkle cheese:</p> Evenly sprinkle the grated
-              cheese over the vegetable casserole.
-            </li>
-            <li className="step">
-              <p className="meal-title">Bake:</p> Place the casserole dish in the preheated
-              oven and bake the vegetable casserole for about 30-35 minutes,
-              until the potatoes are tender and the cheese is golden brown and
-              crispy.
-            </li>
-            <li className="step">
-              <p className="meal-title">Serve:</p> Remove the vegetable casserole from the
-              oven and let it rest for about 5 minutes before serving. Garnish
-              with fresh herbs as desired and serve hot.
-            </li>
-          </ol>
-        </div>
-
-        <p className="note">
-          This veggie casserole is versatile, healthy, and can be prepared in
-          advance. You can vary the types of vegetables according to season and
-          personal preferences. Enjoy your meal!
-        </p>
-
-        <div className="person-selector">
-          <button className="button-minus"
-            onClick={() =>
-              setPersons((prevPersons) => Math.max(prevPersons - 1, 1))
-            }
-          >
-            -
-          </button>
-          <span className="persons">{persons}</span>
-          <button className="button-plus" onClick={() => setPersons((prevPersons) => prevPersons + 1)}>
-            +
-          </button>
-        </div>
-        <div className="putcart-btn">
-        <button className="put-cart" onClick={addToCart}>Add to Cart</button>
-        </div>
-      </div>
-    </section>
-    <Footer/>
+      </section>
+      <Footer />
     </>
   );
 };
