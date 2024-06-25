3be50bbc9cb0739a6c5c11404ba6dfe6d9c1a89f
AdrSpeer <adn.speer@gmail.com>
2024-06-25 09:16:05 +0200

    cart closing tag

--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -52,17 +52,20 @@ const Cart = () => {
     });
 
     const data = await res.json();
-    toast.success("Order confirmed! Check your order status under 'Orders' at any time.", {
-      position: "top-right",
-      autoClose: 6000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
-      // transition: Bounce,
-    });
+    toast.success(
+      "Order confirmed! Check your order status under 'Orders' at any time.",
+      {
+        position: "top-right",
+        autoClose: 6000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        // transition: Bounce,
+      }
+    );
     setCart();
   }
 
@@ -76,36 +79,36 @@ const Cart = () => {
       <section className="cart-container">
         <GoBack title={"Cart"} />
 
-      </div>
-      <div className="cart-items">
-        {cart?.length > 0 ? (
-          cart?.items?.map((item, index) => (
-            <CartItem
-              key={index}
-              imageUrl={item?.productId.image}
-              productName={item?.productId.name}
-              unit={item?.productId.unit}
-              rating={item?.productId.rating}
-              price={item?.productId.price}
-              amount={item?.quantity}
-              productId={item?.productId._id}
-              fetchCart={fetchCart}
-            />
-          ))
-        ) : (
-          <div className="empty-cart">
-            <img src="../../../public/emptyCart.svg" alt="emptycart" />
-            <h4>You haven't added anything to your Cart yet.</h4>
-          </div>
-        )}
-      </div>
-      <button
-        className={cart?.length > 0 ? "btn-green-two" : "hide"}
-        onClick={addToOrder}
-      >
-        Check Out - Total: {calculateTotalPrice(cart?.items)}€
-      </button>
-    </section>
+        <div className="cart-items">
+          {cart?.length > 0 ? (
+            cart?.items?.map((item, index) => (
+              <CartItem
+                key={index}
+                imageUrl={item?.productId.image}
+                productName={item?.productId.name}
+                unit={item?.productId.unit}
+                rating={item?.productId.rating}
+                price={item?.productId.price}
+                amount={item?.quantity}
+                productId={item?.productId._id}
+                fetchCart={fetchCart}
+              />
+            ))
+          ) : (
+            <div className="empty-cart">
+              <img src="../../../public/emptyCart.svg" alt="emptycart" />
+              <h4>You haven't added anything to your Cart yet.</h4>
+            </div>
+          )}
+        </div>
+        <button
+          className={cart?.length > 0 ? "btn-green-two" : "hide"}
+          onClick={addToOrder}
+        >
+          Check Out - Total: {calculateTotalPrice(cart?.items)}€
+        </button>
+      </section>
+    </>
   );
 };
 
