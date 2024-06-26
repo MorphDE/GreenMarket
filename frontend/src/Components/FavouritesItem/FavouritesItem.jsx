5b08e85161d13b2a5a4e019ae4eeccf8d0041ba6
AdrSpeer <adn.speer@gmail.com>
2024-06-26 13:37:43 +0200

    link for detail

--- frontend/src/Components/FavouritesItem/FavouritesItem.jsx
+++ frontend/src/Components/FavouritesItem/FavouritesItem.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
 import { useAuth } from "../../Context/AuthProvider";
 import { backendUrl } from "../../api/api";
 import "./FavouritesItem.css";
 
-const FavouritesItem = ({ id, imageUrl, productName, unit, rating, price, handleDelete }) => {
+const FavouritesItem = ({
+  id,
+  imageUrl,
+  productName,
+  unit,
+  rating,
+  price,
+  handleDelete,
+}) => {
   const fullImageUrl = `${backendUrl}/api/v1/uploads/product-images/${imageUrl}`;
   const { token } = useAuth();
 
@@ -10,7 +19,10 @@ const FavouritesItem = ({ id, imageUrl, productName, unit, rating, price, handle
     <section className="single-item">
       <i className="fa-solid fa-trash-can" onClick={() => handleDelete(id)}></i>
       <div className="item-img">
-        <img src={fullImageUrl} alt={productName} />
+        <Link to={`/product/${id}`}>
+          {" "}
+          <img src={fullImageUrl} alt={productName} />
+        </Link>
       </div>
       <div className="item-content">
         <h1>{productName}</h1>
@@ -18,7 +30,7 @@ const FavouritesItem = ({ id, imageUrl, productName, unit, rating, price, handle
           <i className="fa-solid fa-star"></i>
           <p className="item-rate">{rating}</p>
         </div>
-          <p>Price: ${price.toFixed(2)} </p>
+        <p>Price: ${price.toFixed(2)} </p>
       </div>
     </section>
   );
